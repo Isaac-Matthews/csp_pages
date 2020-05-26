bdf8c817fea72860b3b4dfbb84009fd90a05dd59
Isaac Matthews <I.J.Matthews2@newcastle.ac.uk>
2020-05-26 21:26:59 +0100

    js

--- comment.js
+++ comment.js
@@ -1,3 +1,6 @@
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Javascript has been run";
+  document.getElementById("demo").innerHTML = "Javascript has been run safely";
+  alert("ran js");
 }
+
+document.getElementById("button2").addEventListener('click',myFunction);
